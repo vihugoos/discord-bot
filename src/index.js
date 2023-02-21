a2cbdd4133c97860e0aa27b15d49c475f2ccdb89
Victor Hugo <victorhugoos@live.com>
2023-02-21 04:27:24 -0300

    feat: interaction commands implemented

--- src/index.js
+++ src/index.js
@@ -1,40 +1,65 @@
 require("dotenv").config();
-const { Client, GatewayIntentBits, Partials } = require("discord.js");
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessageReactions,
-    ],
-    partials: [
-        Partials.Message,
-        Partials.GuildMember,
-        Partials.Reaction,
-        Partials.User,
-        Partials.Channel,
-    ],
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const commandsPath = path.join(__dirname, "commands");
+const commandsFiles = fs.readdirSync(commandsPath);
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+for (const file of commandsFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, (c) => {
+    console.log(`\n[discord-bot]: Bot is ready! Logged in as ${c.user.tag}`);
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (interaction.isStringSelectMenu()) {
+        const selected = interaction.values[0];
 
-client.on("ready", () => {
-    console.log("\n[discord-bot]: Bot started with success!");
-});
-
-client.on("messageCreate", (message) => {
-    if (message.author.bot) return;
-
-    if (message.content === "!ping") {
-        message.channel.send(
-            `The bot's ping is estimated at ${client.ws.ping}ms`
-        );
+        if (selected == "javascript") {
+            await interaction.reply(
+                "Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            );
+        } else if (selected == "python") {
+            await interaction.reply(
+                "Documentação do Python: https://www.python.org"
+            );
+        } else if (selected == "csharp") {
+            await interaction.reply(
+                "Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/"
+            );
+        } else if (selected == "discordjs") {
+            await interaction.reply(
+                "Documentação do Discord.js: https://discordjs.guide/#before-you-begin"
+            );
+        }
     }
 
-    if (message.content === "!solicitações abertas") {
-        message.reply("Hi, Dear!");
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.log("Command not found!");
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        interaction.reply("There was an error executing this command!");
     }
 });
+
+client.login(process.env.TOKEN);

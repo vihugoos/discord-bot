3f08dd79c46272ea46f30a145c9e75713db11741
Victor Hugo <victorhugoos@live.com>
2023-02-21 22:51:32 -0300

    feat: commandHandling function created

---
+++ src/commandHandling.js
@@ -0,0 +1,24 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+module.exports = function commandHandling(client) {
+    const commandsPath = path.join(__dirname, "commands");
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+            );
+        }
+    }
+
+    return client;
+};

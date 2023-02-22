ce2b9fe53bdfacf4cdedba6254b49d8af5110468
Victor Hugo <victorhugoos@live.com>
2023-02-21 22:52:59 -0300

    feat(index.js): commandHandling and eventHandling functions implemented

--- src/index.js
+++ src/index.js
@@ -1,78 +1,16 @@
 require("dotenv").config();
 
-const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
-const fs = require("node:fs");
-const path = require("node:path");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const commandHandling = require("./commandHandling");
+const eventHandling = require("./eventHandling");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
+commandHandling(client);
 
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(
-            `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-        );
-    }
-}
-
-client.once(Events.ClientReady, (c) => {
-    console.log(`\n[discord-bot]: Bot is ready! Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (interaction.isStringSelectMenu()) {
-        const selected = interaction.values[0];
-
-        if (selected == "javascript") {
-            await interaction.reply(
-                "Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-            );
-        } else if (selected == "python") {
-            await interaction.reply(
-                "Documentação do Python: https://www.python.org"
-            );
-        } else if (selected == "csharp") {
-            await interaction.reply(
-                "Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/"
-            );
-        } else if (selected == "discordjs") {
-            await interaction.reply(
-                "Documentação do Discord.js: https://discordjs.guide/#before-you-begin"
-            );
-        }
-    }
-
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(
-            `No command matching ${interaction.commandName} was found.`
-        );
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({
-            content: "There was an error while executing this command!",
-            ephemeral: true,
-        });
-    }
-});
+eventHandling(client);
 
 client.login(process.env.TOKEN);
